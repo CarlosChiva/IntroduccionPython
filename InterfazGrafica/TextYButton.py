c4898d5f84a1c6ee80948e31c213721a03ab3c54
Carlos Chiva <Aluchi1821@ieselcaminas.org>
2022-08-04 18:09:24 +0200

    SecondContact on Python to GraphicsInterfaces 2

--- InterfazGrafica/TextYButton.py
+++ InterfazGrafica/TextYButton.py
@@ -3,11 +3,14 @@ from tkinter import *
 root = Tk()
 miFrame = Frame(root, width=1200, height=600)
 miFrame.pack()
+# asignar tipo de variable String
+minombre = StringVar()
 
 nombreLabel = Label(miFrame, text="Nombre: ")
 nombreLabel.grid(row=0, column=0, padx=10, pady=10, sticky="w")
 
-nombreTexto = Entry(miFrame)
+# con textVariable asociamos una variable al contenido de ese contenedor, ese cuadro
+nombreTexto = Entry(miFrame,textvariable=minombre)
 nombreTexto.grid(row=0, column=1, padx=10, pady=10)
 
 apellidoLabel = Label(miFrame, text="Apellido: ")
@@ -23,11 +26,16 @@ comentarioLabel.grid(row=2, column=0, padx=10, pady=10, sticky="w")
 textoComentario = Text(miFrame, width=16, height=5)
 # para barra de scroll se crea un scrollbar al que se le pone de parametros elcontenedor, en comand se le dice a que elemento se añadira y con el metodo de yview o  xview para que eje hará scroll
 scrollVertical = Scrollbar(miFrame, command=textoComentario.yview)
-scrollVertical.grid(row=2,column=2,sticky="nsew")
+scrollVertical.grid(row=2, column=2, sticky="nsew")
 textoComentario.config(yscrollcommand=scrollVertical.set)
 textoComentario.grid(row=2, column=1, padx=10, pady=10)
 
-botonDeEnvio=Button(root,text="Enviar")
+
+def codigoBoton():
+    minombre.set("Yomismo")
+
+# al pulsar el boton se cargara el nombre del set a la variable y a su vez al cuadro
+botonDeEnvio = Button(root, text="Enviar", command=codigoBoton)
 botonDeEnvio.pack()
 
 root.mainloop()
